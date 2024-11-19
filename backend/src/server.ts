ba3d6b8b57ba38eb714483664d01e94c920fc3e1
Petrando <petrandorichard@gmail.com>
2024-11-19 16:53:47 +0800

    can connect to MongoDB Atlas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "dotenv/config"
+import mongoose from "mongoose"
 import express from "express"
+
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
     res.send("Hello, World")
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+const URI = process.env.MONGO_CONNECTION_STRING || "";
+
+mongoose.connect(URI, { dbName: "messages",})
+    .then(()=>{
+        console.log("connected to MongoDB")
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`)
+        })
+    })
+    .catch(console.error)
