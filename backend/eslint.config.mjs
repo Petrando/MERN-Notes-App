ff374ff71ff24cdf361a4d96ee6854b8e3a2cfc9
Petrando <petrandorichard@gmail.com>
2024-11-20 18:23:34 +0800

    create app.ts for better code organization

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,7 +5,13 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",   // Disable no-explicit-any rule
+      "@typescript-eslint/no-unused-vars": "off"     // Disable no-unused-vars rule
+    }
+  },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import "dotenv/config"
+import app from "./app";
 import env from "./utils/validateEnv"
 import mongoose from "mongoose"
-import express from "express"
 
-const app = express()
 const port = env.PORT || 5000
-
-app.get("/", (req, res) => {
-    res.send("Hello, World")
-})
 
 const URI = env.MONGO_CONNECTION_STRING || "";
 
