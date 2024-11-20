d732143f18c4af8ace45f4cf1629c9e24a15674c
Petrando <petrandorichard@gmail.com>
2024-11-20 16:13:54 +0800

    dotenv validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import "dotenv/config"
+import env from "./utils/validateEnv"
 import mongoose from "mongoose"
 import express from "express"
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = env.PORT || 5000
 
 app.get("/", (req, res) => {
     res.send("Hello, World")
 })
 
-const URI = process.env.MONGO_CONNECTION_STRING || "";
+const URI = env.MONGO_CONNECTION_STRING || "";
 
 mongoose.connect(URI, { dbName: "messages",})
     .then(()=>{
