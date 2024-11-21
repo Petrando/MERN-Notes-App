d43275fd4cf6c0068438bb119a9232048be03cfe
Petrando <petrandorichard@gmail.com>
2024-11-21 16:16:07 +0800

    feat: frontend can display list of notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Col, Row, Spinner } from "react-bootstrap";
+import Note from './components/Note';
+import styles from "./styles/NotesPage.module.css";
+import { Note as NoteModel } from './models/note';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [notesLoading, setNotesLoading] = useState(true);
+  const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
+
+  useEffect(() => {
+    async function loadNotes() {
+        try {
+            setShowNotesLoadingError(false);
+            setNotesLoading(true);
+            const res = await fetch("/api/notes", { method: "GET" })
+            const notes = await res.json()            
+            setNotes(notes);
+        } catch (error) {
+            console.error(error);
+            setShowNotesLoadingError(true);
+        } finally {
+            setNotesLoading(false);
+        }
+    }
+    loadNotes();
+  }, []);
+
+  {/*onNoteClicked={setNoteToEdit}
+                    onDeleteNoteClicked={deleteNote}*/}
+  const notesGrid =
+    <Row xs={1} md={2} xl={3} className={`g-4 ${styles.notesGrid}`}>
+        {notes.map(note => (
+            <Col key={note._id}>
+                <Note
+                    note={note}
+                    className={styles.note}
+                    
+                />
+            </Col>
+        ))}
+    </Row>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!notesLoading && !showNotesLoadingError &&
+          <>
+              {notes.length > 0
+                  ? notesGrid
+                  : <p>You don't have any notes yet</p>
+              }
+          </>
+      }
     </div>
   );
 }
