585618311eea0ef00e4f6d879d16863b97e95854
Petrando <petrandorichard@gmail.com>
2024-11-22 16:33:13 +0800

    backend login logout routes for user tested

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -46,3 +46,46 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
         next(error);
     }
 };
+
+interface LoginBody {
+    username?: string,
+    password?: string,
+}
+
+export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        if (!username || !password) {
+            throw createHttpError(400, "Parameters missing");
+        }
+
+        const user = await UserModel.findOne({ username: username }).select("+password +email").exec();
+
+        if (!user) {
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        req.session.userId = user._id;
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const logout: RequestHandler = (req, res, next) => {
+    req.session.destroy(error => {
+        if (error) {
+            next(error);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+};
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -7,4 +7,8 @@ const router = express.Router();
 
 router.post("/signup", UserController.signUp);
 
+router.post("/login", UserController.login)
+
+router.post("/logout", UserController.logout)
+
 export default router
